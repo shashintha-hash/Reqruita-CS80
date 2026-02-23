98a33323a7c63103a316536cd5e56ecf8ca11cd7
Ishan-malinda <ishanmalindhaims@gmail.com>
2026-02-23 16:53:19 +0530

    Added URLs for Dashboard

--- reqruita-desktop-app/src/config.js
+++ reqruita-desktop-app/src/config.js
@@ -1,2 +1,18 @@
 /** Add your own backend URL */
 export const BACKEND_URL = "http://172.20.10.4:3001";
+
+//export const BACKEND_URL = "http://172.20.10.4:3001"; - Upstream Server
+
+
+// If you want to use the local server, uncomment the following line:
+//export const BACKEND_URL = "http://localhost:3001"; - Local Server
+
+
+
+
+
+
+
+
+
+
