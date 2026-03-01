f0233ce9357bc9c08cd816e98ea2c612a6b62071
Shashintha <shashinthaishan@gmail.com>
2026-03-01 12:57:36 +0530

    Added the toggle chat pannel and update the chat button according to that function

--- reqruita-desktop-app/src/config.js
+++ reqruita-desktop-app/src/config.js
@@ -1,14 +1,6 @@
 
 
-export const BACKEND_URL = "http://172.20.10.4:3001";
-
-<<<<<<< HEAD
-=======
-//export const BACKEND_URL = "http://172.20.10.4:3001"; - Upstream Server
-
-
-// If you want to use the local server, uncomment the following line:
-//export const BACKEND_URL = "http://localhost:3001"; - Local Server
+export const BACKEND_URL = "http://localhost:3001"; 
 
 
 
@@ -18,5 +10,5 @@ export const BACKEND_URL = "http://172.20.10.4:3001";
 
 
 
->>>>>>> upstream/main
+
 
