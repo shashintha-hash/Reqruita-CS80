e5da361153d08d737437e2df4e5f257a63c7f541
Thenuja <contact.thenujahansana@gmail.com>
2026-03-14 04:58:08 +0530

    resolve the merge conflict in config.js & index.html

--- reqruita-desktop-app/src/config.js
+++ reqruita-desktop-app/src/config.js
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
 
-
-export const BACKEND_URL = "http:// 192.168.1.26:3001"; 
-
-
-
-
-
-=======
 /** Add your own backend URL */
-//export const BACKEND_URL = "http://172.20.10.4:3001"; // Upstream Server
-//export const BACKEND_URL = "http://172.20.10.10:3001"; // Alternate Upstream
+// If you want to use the local server, use:
+export const BACKEND_URL = "http://localhost:3001";
 
-// If you want to use the local server, uncomment the following line:
-export const BACKEND_URL = "http://localhost:3001";  //- Local Server
->>>>>>> upstream/main
 
 
 
