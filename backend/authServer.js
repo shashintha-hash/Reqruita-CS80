b4068f8376086ccc546dab17a45e1b4cb7ea2585
Thenuja <contact.thenujahansana@gmail.com>
2026-03-15 04:50:39 +0530

    resolve port conflict

--- backend/authServer.js
+++ backend/authServer.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
