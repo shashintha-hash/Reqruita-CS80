8ec84eb1deb0cb36125fa2210269c876066309d2
Thenuja <contact.thenujahansana@gmail.com>
2026-03-18 19:43:22 +0530

    fixed video box renders properly now

--- backend/InterviewRemark.js
+++ backend/InterviewRemark.js
@@ -1,11 +1,11 @@
-const mogoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const interviewRemarkSchema = new mongoose.Schema ({
+const interviewRemarkSchema = new mongoose.Schema({
     interviewId: { type: String, required: true },
-    interviewerId: { type: String, required: true },
-    remark: { type: String, required: true },
-    rating: Number,
-    createdAt: { type: Date, default: Date.now },
+    participantId: { type: String, required: true },
+    remark: { type: String, required: true }
+}, { 
+    timestamps: true 
 });
 
-module.exports=mongoose.model("InterviewRemark", interviewRemarkSchema);
+module.exports = mongoose.model("InterviewRemark", interviewRemarkSchema);
